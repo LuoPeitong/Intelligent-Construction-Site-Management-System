a9a2e6cddb49817c5be68abf9d0f8dd944324938
company <company@jpjt>
2023-04-26 16:53:23 +0800

    没做完

--- web_ssms/src/components/func/RegionalManagement.vue
+++ web_ssms/src/components/func/RegionalManagement.vue
@@ -1,13 +1,52 @@
 <template>
-<span>非监控区域判定 人员终端 离线后 是否是处于 工作区间状态或进入禁止区域</span>
+  <div id="container">
+    <el-form :inline="true" :model="formArea" class="demo-form-inline">
+      <el-form-item label="区域名称">
+        <el-input v-model="formArea.name" placeholder="区域名称"></el-input>
+      </el-form-item>
+      <el-form-item label="区域类型">
+        <el-select v-model="formArea.class" placeholder="区域类型">
+          <el-option label="活动区域" value="shanghai"></el-option>
+          <el-option label="禁入区域" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="区域坐标">
+        <el-input v-model="formArea.region" placeholder="区域坐标" class="regionInput"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'RegionalManagement'
+  name: 'RegionalManagement',
+  data() {
+    return {
+      formArea: {
+        name: '',
+        region: '',
+        class: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style>
+  #container {
+    width: 100% !important;
+    height: 100vh !important;
+    margin-right: 10px !important;
+  }
+  .regionInput {
+    width: 400px;
+  }
 </style>
